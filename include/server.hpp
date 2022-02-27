e98f8022a2f6748b15a6a5187c7c80c0686c036c
Cadeusept <matwey.likhota@gmail.com>
2022-02-27 20:17:02 +0300

    work_in_progress

--- include/server.hpp
+++ include/server.hpp
@@ -19,16 +19,45 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ip/tcp.hpp>
 
+extern boost::asio::io_service service;
+
 class talk_to_client {
- private:
-  int socket;
  public:
-  int sock() {
-    return this->socket;
+  boost::asio::ip::tcp::socket _sock{service};
+  boost::posix_time::ptime now;
+  bool _status = true;
+  bool _initial = true;
+  uint64_t _clients_num = 0;
+  std::string _name, _request;
+
+  void read_request() {
+    boost::asio::streambuf buffer{};
+    read_until(_sock, buffer, "\n");
+    std::string request((std::istreambuf_iterator<char>(&buffer)),
+                        std::istreambuf_iterator<char>());
+    _request = request;
+  }
+
+  void write_reply(std::string reply) {
+    reply += "\r\n";
+    _sock.write_some(boost::asio::buffer(reply));
+  }
+
+  void ping() {
+    write_reply("ping_ok\n");
+  }
+
+  void communicate() {
+    try {
+
+    }
+    catch(...) {
+
+    }
   }
 };
 
-static volatile boost::recursive_mutex mutex;
+//extern boost::recursive_mutex mutex;
 
 void accept_thread();
 
--- sources/server.cpp
+++ sources/server.cpp
@@ -4,26 +4,32 @@
 
 #include <server.hpp>
 
-boost::recursive_mutex mutex;
-std::vector<std::shared_ptr<talk_to_client>> clients;
+boost::asio::io_service service;
+static boost::asio::ip::tcp::acceptor acceptor
+        {service, boost::asio::ip::tcp::endpoint
+        {boost::asio::ip::tcp::v4(), 8001}};
+std::vector<std::string> client_list;
+typedef boost::shared_ptr<talk_to_client> client_ptr;
+std::vector<client_ptr> clients;
+boost::recursive_mutex cli_mutex;
 
 void accept_thread() {
-    boost::asio::ip::tcp::acceptor acceptor{service,
-              boost::asio::ip::tcp::endpoint{boost::asio::ip::tcp::v4{}, 8001}};
+    fflush(stdout);
     while (true) {
-        auto client = std::make_shared<talk_to_client>(); // talk_to_client - user defined class
-        acceptor.accept(client->sock());
-        boost::recursive_mutex::scoped_lock lock{mutex};
-        clients.push_back(client);
+        client_ptr one(new talk_to_client); // talk_to_client - user defined class
+        acceptor.accept(one->_sock);
+        boost::recursive_mutex::scoped_lock lock{cli_mutex};
+        clients.push_back(one);
     }
 }
 
 void handle_clients_thread() {
     while (true) {
-        std::this_thread::sleep(std::chrono::milliseconds{1});
-        boost::recursive_mutex::scoped_lock lock{mutex};
+        boost::this_thread::sleep(boost::posix_time::millisec(1));
+        boost::recursive_mutex::scoped_lock lock{cli_mutex};
         for (auto& client : clients) {
-            // for each client calling answer_to_client();
+          client.communicate();
+          // for each client calling answer_to_client();
         }
         // and then erase clients that timed out
       }
