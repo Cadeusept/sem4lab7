4a8eef82e258853fb6ce9e02c0ee29b4628826d6
Cadeusept <matwey.likhota@gmail.com>
2022-03-14 12:49:00 +0300

    work_in_progress

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,8 +1,19 @@
 #include "server.hpp"
+#include "client.hpp"
 
 int main(int argc, char* argv[]) {
   boost::thread_group threads;
-  threads.create_thread(accept_thread);
-  threads.create_thread(handle_clients_thread);
-  threads.join_all();
+  if (argc == 2) {
+    if (atoi(argv[1]) == 1) { //server
+      threads.create_thread(accept_thread);
+      threads.create_thread(handle_clients_thread);
+      threads.join_all();
+    } else if (atoi(argv[1]) == 2) {  // client
+      threads.create_thread(run_client);
+    } else {
+      return 1;
+    }
+  } else {
+    return 2;
+  }
 }
